ffa3b075367c161c160b8806391263bd54ab4212
alienigena264 <jgarciaot@unal.edu.co>
2023-04-28 17:47:54 -0500

    Mejoramiento lectura de codigo/conversion a metodos y arreglo bug al abrir un teclado

--- lib/view/login_vista.dart
+++ lib/view/login_vista.dart
@@ -11,112 +11,83 @@ class _LoginVistaState extends State<LoginVista> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        color: const Color(0xffF6F6F6),
-        child: Column(
-          children: [
-            Stack(
-              children: [
-                // Contenedor con texto y fondo
-                SizedBox(
-                  height: 186.0,
-                  child: Container(
-                    decoration: const BoxDecoration(
-                      color: Color(
-                          0xff00B5C0), // Establece el color de fondo del contenedor con el texto
-                      borderRadius: BorderRadius.only(
-                        bottomLeft: Radius.circular(50.0),
-                      ),
-                    ),
-                    child: const Center(
-                      child: FittedBox(
-                        fit: BoxFit.scaleDown,
-                        child: Text(
-                          'Ingresar', // el texto que quieres mostrar
-                          style: TextStyle(
-                              color:
-                                  Colors.white, // Establece el color del texto
-                              fontSize: 55.0, // Establece el tamaño del texto
-                              fontFamily: "Inder",
-                              fontWeight: FontWeight.normal),
-                        ),
-                      ),
-                    ),
+      body: SingleChildScrollView(
+        child: Container(
+          color: const Color(0xffF6F6F6),
+          child: Column(
+            children: [
+              Stack(
+                children: [
+                  // Contenedor con texto y fondo
+                  barraSuperior_(),
+                ],
+              ),
+              const SizedBox(
+                height: 33,
+              ),
+              inputEmail_(),
+              inputPassword_(),
+              Row(
+                children: [
+                  const SizedBox(
+                    width: 25,
                   ),
-                ),
-              ],
-            ),
-            const SizedBox(
-              height: 33,
-            ),
-            inputEmail_(),
-            inputPassword_(),
-            Row(
-              children: [
-                const SizedBox(
-                  width: 25,
-                ),
-                TextButton(
-                    style:
-                        TextButton.styleFrom(padding: const EdgeInsets.all(1)),
-                    onPressed: () => print('Olvido la password'),
-                    child: const Text(
-                      '¿Olvidó su contraseña?',
-                      style: TextStyle(
-                          color: Colors.black,
-                          fontSize: 15,
-                          fontFamily: "Inder",
-                          fontWeight: FontWeight.bold),
-                    )),
-              ],
-            ),
-            const SizedBox(
-              height: 20,
-            ),
-            ElevatedButton(
-                style: ElevatedButton.styleFrom(
-                  backgroundColor: const Color(0xffEFB810),
-                  minimumSize: const Size(340,
-                      55), // double.infinity is the width and 30 is the height
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(14.0),
+                  olvidoPassButtom_(),
+                ],
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              inicioSesionButtom_(),
+              const SizedBox(
+                height: 15,
+              ),
+              Row(
+                children: [
+                  const SizedBox(
+                    width: 55,
                   ),
-                ),
-                onPressed: verificar_(),
-                child: const Text(
-                  "Iniciar sesión",
-                  style: TextStyle(color: Colors.black, fontSize: 18),
-                )),
-            const SizedBox(
-              height: 15,
-            ),
-            Row(
-              children: [
-                const SizedBox(
-                  width: 55,
-                ),
-                const Text(
-                  '¿No tienes una cuenta?',
-                  style: TextStyle(fontWeight: FontWeight.bold),
-                ),
-                TextButton(
-                    style: TextButton.styleFrom(
-                        padding: const EdgeInsets.only(left: 10)),
-                    onPressed: () => print('Registrarse'),
-                    child: const Text(
-                      'Regístrate aquí',
-                      style: TextStyle(
-                          color: Color(0xffEFB810),
-                          fontSize: 15,
-                          fontFamily: "Inder",
-                          fontWeight: FontWeight.bold),
-                    )),
-              ],
-            )
-          ],
+                  const Text(
+                    '¿No tienes una cuenta?',
+                    style: TextStyle(fontWeight: FontWeight.bold),
+                  ),
+                  registrarseButtom_(),
+                ],
+              )
+            ],
+          ),
         ),
       ),
     );
+  }
+
+  SizedBox barraSuperior_() {
+    return SizedBox(
+                height: 186.0,
+                child: Container(
+                  decoration: const BoxDecoration(
+                    color: Color(
+                        0xff00B5C0), // Establece el color de fondo del contenedor con el texto
+                    borderRadius: BorderRadius.only(
+                      bottomLeft: Radius.circular(50.0),
+                    ),
+                  ),
+                  child: const Center(
+                    child: FittedBox(
+                      fit: BoxFit.scaleDown,
+                      child: Text(
+                        'Ingresar', // el texto que quieres mostrar
+                        style: TextStyle(
+                            color:
+                                Colors.white, // Establece el color del texto
+                            fontSize: 55.0, // Establece el tamaño del texto
+                            fontFamily: "Inder",
+                            fontWeight: FontWeight.normal),
+                      ),
+                    ),
+                  ),
+                ),
+              );
   }
 
   inputEmail_() {
@@ -126,6 +97,8 @@ class _LoginVistaState extends State<LoginVista> {
       child: const TextField(
         keyboardType: TextInputType.emailAddress,
         decoration: InputDecoration(
+          filled: true,
+          fillColor: Colors.white,
           border: OutlineInputBorder(
             borderRadius: BorderRadius.all(Radius.circular(8.0)),
           ),
@@ -146,6 +119,8 @@ class _LoginVistaState extends State<LoginVista> {
       child: const TextField(
         obscureText: true,
         decoration: InputDecoration(
+          filled: true,
+          fillColor: Colors.white,
           border: OutlineInputBorder(
             borderRadius: BorderRadius.all(Radius.circular(8.0)),
           ),
@@ -159,13 +134,66 @@ class _LoginVistaState extends State<LoginVista> {
     );
   }
 
-  verificar_() {
-    return () {
-      print("Verificar");
-    };
+  TextButton olvidoPassButtom_() {
+    return TextButton(
+        style: TextButton.styleFrom(padding: const EdgeInsets.all(1)),
+        onPressed: recuperarPassword_(),
+        child: const Text(
+          '¿Olvidó su contraseña?',
+          style: TextStyle(
+              color: Colors.black,
+              fontSize: 15,
+              fontFamily: "Inder",
+              fontWeight: FontWeight.bold),
+        ));
+  }
+
+  ElevatedButton inicioSesionButtom_() {
+    return ElevatedButton(
+        style: ElevatedButton.styleFrom(
+          backgroundColor: const Color(0xffEFB810),
+          minimumSize: const Size(
+              340, 55), // double.infinity is the width and 30 is the height
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(14.0),
+          ),
+        ),
+        onPressed: verificar_(),
+        child: const Text(
+          "Iniciar sesión",
+          style: TextStyle(color: Colors.black, fontSize: 18),
+        ));
+  }
+
+  TextButton registrarseButtom_() {
+    return TextButton(
+        style: TextButton.styleFrom(padding: const EdgeInsets.only(left: 10)),
+        onPressed: registrarse_(),
+        child: const Text(
+          'Regístrate aquí',
+          style: TextStyle(
+              color: Color(0xffEFB810),
+              fontSize: 15,
+              fontFamily: "Inder",
+              fontWeight: FontWeight.bold),
+        ));
   }
 
   recuperarPassword_() {
-    print('Se recupero');
+    return () {
+      print('Se recupero');
+    };
+  }
+
+  verificar_() {
+    return () {
+      print("Inicio sesion");
+    };
+  }
+
+  registrarse_() {
+    return () {
+      print('Se registro');
+    };
   }
 }
