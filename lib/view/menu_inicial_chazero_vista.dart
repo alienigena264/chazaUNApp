c081ef702f2e77f5a13a8cd2bb92e124217270a9
CMachadoG <cmachado@unal.edu.co>
2023-05-03 13:47:11 -0500

    Menú chazero sin bd

--- lib/view/menu_inicial_chazero_vista.dart
+++ lib/view/menu_inicial_chazero_vista.dart
@@ -10,40 +10,46 @@ class MenuChazero extends StatefulWidget {
 class _MenuChazeroState extends State<MenuChazero> {
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Column(
-        children: [
-          Stack(
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: Container(
+          color: Colors.white,
+          child: Column(
             children: [
-              // Contenedor con texto y fondo
-              barraSuperior_(),
+              Stack(
+                children: [
+                  barraSuperior_(),
+                ],
+              ),
+              Container(
+                height: 550, // Tamaño fijo
+                child: ListView.builder(
+                  shrinkWrap: true,
+                  itemCount: chazas.length,
+                  itemBuilder: (BuildContext context, int index) {
+                    return SizedBox(
+                      height: 162,
+                      width: MediaQuery.of(context).size.width * 0.9,
+                      child: infoChaza_(
+                          chazas[index][0], chazas[index][1], chazas[index][2],
+                          chazas[index][3], chazas[index][4]),
+                    );
+                  },
+                ),
+              ),
             ],
           ),
-          Expanded(
-            child: ListView.builder(
-              itemCount: chazas.length,
-              itemBuilder: (BuildContext context, int index) {
-                return SizedBox(
-                  height: 120,
-                    width: MediaQuery.of(context).size.width * 0.8// Altura fija para todas las tarjetas
-                  ,child: infoChaza_(chazas[index][0], chazas[index][1], chazas[index][2], chazas[index][3]),
-                );
-              },
-            ),
-          ),
-        ],
+        ),
       ),
     );
   }
-}
 
   SizedBox barraSuperior_() {
     return SizedBox(
       height: 186.0,
       child: Container(
         decoration: const BoxDecoration(
-          color: Color(
-              0xff00B5C0),
+          color: Color(0xff00B5C0),
           // Establece el color de fondo del contenedor con el texto
           borderRadius: BorderRadius.only(
             bottomLeft: Radius.circular(50.0),
@@ -55,11 +61,10 @@ class _MenuChazeroState extends State<MenuChazero> {
             child: Text(
               'Chazero', // el texto que quieres mostrar
               style: TextStyle(
-                  color:
-                  Colors.white, // Establece el color del texto
-                  fontSize: 55.0, // Establece el tamaño del texto
-                  fontFamily: "Inder",
-                  fontWeight: FontWeight.normal),
+                color: Colors.white, // Establece el color del texto
+                fontSize: 35.0, // Establece el tamaño del texto
+                fontFamily: "Inder",
+              ),
             ),
           ),
         ),
@@ -67,34 +72,41 @@ class _MenuChazeroState extends State<MenuChazero> {
     );
   }
 
-  Card infoChaza_(String nombre, String ubicacion, String puntuacion, String pago) {
+  Card infoChaza_(String nombre, String ubicacion, String puntuacion,
+      String pago, String imagen) {
     return Card(
       child: Column(
-        mainAxisSize: MainAxisSize.min,
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        mainAxisSize: MainAxisSize.max,
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
         children: <Widget>[
           Row(
-            mainAxisSize: MainAxisSize.min,
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            mainAxisSize: MainAxisSize.max,
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
             children: [
               Column(
-                mainAxisSize: MainAxisSize.min,
+                mainAxisSize: MainAxisSize.max,
                 mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 children: [
                   Text(
                     nombre, // Nombre de la chaza
                     style: TextStyle(
-                        color:
-                        Color(0xff242424),
-                        fontSize: 24.0,
+                        color: Color(0xff242424),
+                        fontSize: 22.0,
                         fontFamily: "Inder",
                         fontWeight: FontWeight.normal),
                   ),
                   rowUbicacion_(ubicacion),
-                  rowPuntuacion_(ubicacion),
+                  rowPuntuacion_(puntuacion),
                 ],
               ),
-              columnFotoYPagoChaza_(pago),
+              columnFotoYPagoChaza_(imagen, pago),
+            ],
+          ),
+          Row( mainAxisSize: MainAxisSize.max,
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [
+                botonHorarios(),
+              botonPersonal()
             ],
           )
         ],
@@ -104,15 +116,15 @@ class _MenuChazeroState extends State<MenuChazero> {
 
   Row rowUbicacion_(String ubicacion) {
     return Row(
+      mainAxisSize: MainAxisSize.max,
       mainAxisAlignment: MainAxisAlignment.start,
       children: <Widget>[
         Icon(Icons.location_on_rounded, color: Color(0xff919191), size: 16.0),
         Text(
           ubicacion,
           style: TextStyle(
-              color:
-              Color(0xff919191),
-              fontSize: 14.0,
+              color: Color(0xff919191),
+              fontSize: 14.5,
               fontFamily: "Inder",
               fontWeight: FontWeight.normal),
         ),
@@ -122,15 +134,19 @@ class _MenuChazeroState extends State<MenuChazero> {
 
   Row rowPuntuacion_(String puntuacion) {
     return Row(
+      mainAxisSize: MainAxisSize.max,
       mainAxisAlignment: MainAxisAlignment.start,
       children: <Widget>[
-        Icon(Icons.star, color: Color(0xffEFB810), size: 16.0,),
+        Icon(
+          Icons.star,
+          color: Color(0xffEFB810),
+          size: 16.0,
+        ),
         Text(
           puntuacion,
           style: TextStyle(
-              color:
-              Color(0xff919191),
-              fontSize: 14.0,
+              color: Colors.black,
+              fontSize: 16.0,
               fontFamily: "Inder",
               fontWeight: FontWeight.normal),
         ),
@@ -138,29 +154,91 @@ class _MenuChazeroState extends State<MenuChazero> {
     );
   }
 
-  Column columnFotoYPagoChaza_(String pago) {
+  Column columnFotoYPagoChaza_(String imagen, String pago) {
     return Column(
-      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      mainAxisSize: MainAxisSize.max,
+      mainAxisAlignment: MainAxisAlignment.spaceAround,
       children: [
-        //Aqui iría la foto de la chaza sacada de la bd
+        const SizedBox(
+          height: 5,
+        ),
+        ClipRRect(
+          borderRadius: BorderRadius.circular(8.0),
+          // Ajusta el radio del borde según tus necesidades
+          child: Image(
+            image: NetworkImage(imagen),
+            height: 61.0,
+          ),
+        ),
         Text(
           pago + " / Hora",
           style: TextStyle(
-              color:
-              Color(0xff7B1FA2),
+              color: Color(0xff7B1FA2),
               fontSize: 13.0,
               fontFamily: "Inder",
               fontWeight: FontWeight.normal),
         ),
-        Icon(Icons.more_horiz, color: Color(0xffEFB810ff919191), size: 24.0,),
+        Icon(
+          Icons.more_horiz,
+          color: Color(0xffEFB810ff919191),
+          size: 24.0,
+        ),
       ],
     );
   }
+  ElevatedButton botonHorarios(){
+    return ElevatedButton(style: ElevatedButton.styleFrom(
+      backgroundColor: const Color(0xffEFB810),
+      minimumSize: const Size(
+          132, 39), // double.infinity is the width and 30 is the height
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(6.0),
+      ),
+    ),
+        onPressed: (){},
+        child: const Text(
+          "Horarios",
+          style: TextStyle(color: Colors.black, fontSize: 16, fontFamily: "Inder"),
+        ));
+  }
+  ElevatedButton botonPersonal(){
+    return ElevatedButton(style: ElevatedButton.styleFrom(
+      backgroundColor: const Color(0xffEFB810),
+      minimumSize: const Size(
+          132, 39), // double.infinity is the width and 30 is the height
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(6.0),
+      ),
+    ),
+        onPressed: (){},
+        child: const Text(
+          "Personal",
+          style: TextStyle(color: Colors.black, fontSize: 16, fontFamily: "Inder"),
+        ));
+  }
+}
 
-  //Ejemplo para ver si funciona una card por chaza
-  List<List<String>> chazas = [
-    ["Sex-Chaza", "Frente a humanas", "3.5", "12000"],
-    ["Los perros del calvo HP", "Junto al FEM", "5.0", "8000"],
-    ["Calcas a 800", "La playita", "4.4", "5000"]
-  ];
-
+//Ejemplo para ver si funciona una card por chaza
+List<List<String>> chazas = [
+  [
+    "Sex-Chaza",
+    "Frente a humanas",
+    "3.5",
+    "12000",
+    "https://flutter.github.io/assets-for-api-docs/assets/widgets/owl-2.jpg"
+  ],
+  [
+    "Los perros del calvo HP",
+    "Junto al FEM",
+    "5.0",
+    "8000",
+    "https://flutter.github.io/assets-for-api-docs/assets/widgets/owl-3.jpg"
+  ],
+  [
+    "Calcas a 800",
+    "La playita",
+    "4.4",
+    "5000",
+    "https://flutter.github.io/assets-for-api-docs/assets/widgets/owl.jpg"
+  ]
+];
