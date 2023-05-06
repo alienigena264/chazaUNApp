0aaec63f4612279c30c21761da90aeb7f3b7ad96
alienigena264 <jgarciaot@unal.edu.co>
2023-05-06 16:23:46 -0500

    Modificacion archivo de ventana en progreso

--- lib/view/Sprint2/configuracion_vista.dart
+++ lib/view/Sprint2/configuracion_vista.dart
@@ -1,5 +1,6 @@
 import 'package:chazaunapp/view/colors.dart';
 import 'package:flutter/material.dart';
+
 class ConfiguracionVista extends StatefulWidget {
   const ConfiguracionVista({super.key});
 
@@ -10,15 +11,29 @@ class ConfiguracionVista extends StatefulWidget {
 class _ConfiguracionVistaState extends State<ConfiguracionVista> {
   @override
   Widget build(BuildContext context) {
-    return  Scaffold(
-      backgroundColor: colorBackground,
-      body: Center(child:Column(
-        children: const [
-          SizedBox(height: 380),
-          Icon(Icons.design_services_outlined),
-          Text('Estamos trabajando en esta funcionalidad'),
-        ],
-      ),)
-    );
+    return Scaffold(
+        backgroundColor: colorBackground,
+        body: Center(
+          child: Column(
+            children: const [
+              SizedBox(height: 320),
+              Icon(
+                Icons.design_services_outlined,
+                size: 80,
+                color: colorPrincipal,
+              ),
+              Center(
+                child: Text(
+                  '    Estamos trabajando en esta\n              funcionalidad',
+                  style:
+                    TextStyle(color: Colors.black,
+                    fontWeight: FontWeight.bold,
+                    fontSize: 22
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ));
   }
 }
