d34f6f63a5898baaca44417b2e8d3dced8bd514f
alienigena264 <jgarciaot@unal.edu.co>
2023-05-10 09:52:30 -0500

    Agregar funcionaliodad conexion con base de datos para la pantalla de login (solo con correo)

---
+++ lib/Controller/Login_Controller.dart
@@ -0,0 +1,5 @@
+
+import 'package:flutter/cupertino.dart';
+
+TextEditingController correoController = TextEditingController(text: '');
+TextEditingController contrasenaController = TextEditingController(text: '');
