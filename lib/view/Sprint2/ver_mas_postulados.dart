e2ced194e9ab4366c696b8eeadc8bfb5ddc2233e
hericep <114833754+hericep@users.noreply.github.com>
2023-06-06 10:43:20 -0500

    Update ver_mas_postulados.dart

--- lib/view/Sprint2/ver_mas_postulados.dart
+++ lib/view/Sprint2/ver_mas_postulados.dart
@@ -119,7 +119,9 @@ class VerMasPostulados extends StatelessWidget {
                                   ),
                                 ),
                                 const SizedBox(width: 107.0),
-                                Expanded(child: Text(snapshot.data?['correo'])),
+                                Expanded(
+                                    child: Text(
+                                        snapshot.data!['Lunes'].toString())),
                               ],
                             ),
                             const SizedBox(height: 25.0),
@@ -132,7 +134,9 @@ class VerMasPostulados extends StatelessWidget {
                                   ),
                                 ),
                                 const SizedBox(width: 100.0),
-                                Expanded(child: Text(snapshot.data?['correo'])),
+                                Expanded(
+                                    child: Text(
+                                        snapshot.data!['Martes'].toString())),
                               ],
                             ),
                             const SizedBox(height: 25.0),
@@ -145,7 +149,9 @@ class VerMasPostulados extends StatelessWidget {
                                   ),
                                 ),
                                 const SizedBox(width: 82.0),
-                                Expanded(child: Text(snapshot.data?['correo'])),
+                                Expanded(
+                                    child: Text(
+                                        snapshot.data!['Miercoles'].toString())),
                               ],
                             ),
                             const SizedBox(height: 25.0),
@@ -158,7 +164,9 @@ class VerMasPostulados extends StatelessWidget {
                                   ),
                                 ),
                                 const SizedBox(width: 101.0),
-                                Expanded(child: Text(snapshot.data?['correo'])),
+                                Expanded(
+                                    child: Text(
+                                        snapshot.data!['Jueves'].toString())),
                               ],
                             ),
                             const SizedBox(height: 25.0),
@@ -171,7 +179,9 @@ class VerMasPostulados extends StatelessWidget {
                                   ),
                                 ),
                                 const SizedBox(width: 98.0),
-                                Expanded(child: Text(snapshot.data?['correo'])),
+                                Expanded(
+                                    child: Text(
+                                        snapshot.data!['Viernes'].toString())),
                               ],
                             ),
                             const SizedBox(height: 25.0),
@@ -184,7 +194,9 @@ class VerMasPostulados extends StatelessWidget {
                                   ),
                                 ),
                                 const SizedBox(width: 98.0),
-                                Expanded(child: Text(snapshot.data?['correo'])),
+                                Expanded(
+                                    child: Text(
+                                        snapshot.data!['Sabado'].toString())),
                               ],
                             ),
                             const SizedBox(height: 50.0),
