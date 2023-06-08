4c888e78499bb4268a9bef906dba5a8a830fc74c
hericep <114833754+hericep@users.noreply.github.com>
2023-06-07 19:46:32 -0500

    Update ver_mas_postulados.dart

--- lib/view/Sprint2/ver_mas_postulados.dart
+++ lib/view/Sprint2/ver_mas_postulados.dart
@@ -206,7 +206,7 @@ class VerMasPostulados extends StatelessWidget {
                 onPressed: () {
                   // Lógica cuando se presiona el botón "Contratar"
                   String nombre = snapshot.data?['nombres'];
-                  String mensajeboton=' ha sido acaptado';
+                  String mensajeboton=' ha sido contratad@';
                   String textorechazado=nombre + mensajeboton;
                   mostrarMensaje(context,textorechazado);
                 },
@@ -219,7 +219,7 @@ class VerMasPostulados extends StatelessWidget {
                 onPressed: () {
                   // Lógica cuando se presiona el botón "Rechazar"
                   String nombre = snapshot.data?['nombres'];
-                  String mensajeboton=' ha sido rechazado';
+                  String mensajeboton=' ha sido rechazad@';
                   String textoaceptado=nombre + mensajeboton;
                   mostrarMensaje(context, textoaceptado);
                 },
