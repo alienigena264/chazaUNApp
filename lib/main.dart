93acad1592065d6afa26adeeda809f9ced89812a
brrodriguezd <brrodriguezd@unal.edu.co>
2023-06-16 09:37:00 -0500

    Analytics

--- lib/main.dart
+++ lib/main.dart
@@ -29,6 +29,7 @@ void main() async {
     statusBarColor: Colors.transparent,
   ));
   await Firebase.initializeApp(
+    name: 'ChazaUNApp',
     options: DefaultFirebaseOptions.currentPlatform,
   );
   runApp(const MyApp());
